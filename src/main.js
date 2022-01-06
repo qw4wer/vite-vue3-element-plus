62a4c8867ab822bc6462f382c6180d41f2c615c3
qw4wer <qw5wer@gmail.com>
2022-01-06 16:40:09 +0800

    init

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import {fas} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
 import apis from './apis'
-import {ElMessage} from 'element-plus'
+import {ElMessage,ElMessageBox} from 'element-plus'
 import router from './router'
 import store from './store'
 import 'element-plus/es/components/message/style/css'
+import 'element-plus/es/components/message-box/style/css'
 
 library.add(fas)
 const app = createApp(App)
@@ -18,3 +19,4 @@ app.component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
 app.config.globalProperties.$apis = apis
 app.config.globalProperties.$message = ElMessage
+app.config.globalProperties.$confirm = ElMessageBox.confirm
