bfc680a2ad7297230a0fbfbef30ada22cbaaca9e
qw4wer <qw5wer@gmail.com>
2022-07-06 16:03:23 +0800

    文件夹管理预提交

---
+++ src/views/yunpan/dir/dir-form.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-form ref="form" :model="dirFormData" :rules="rules" label-width="80px">
+    <el-form-item label="文件夹名称" prop="dirName">
+      <el-input v-model="dirFormData.dirName"></el-input>
+    </el-form-item>
+    <el-form-item label="文件夹路径" prop="path">
+      <el-input v-model="dirFormData.path"></el-input>
+    </el-form-item>
+    <el-form-item label="文件夹描述" prop="describe">
+      <el-input v-model="dirFormData.describe"></el-input>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+import formHandle from "/src/utils/form.handle.js"
+
+export default {
+  name: 'dir-form',
+  props: ['dirFormData'],
+  setup(props) {
+    const { validate, clear } = formHandle();
+    return {
+      validate,
+      clear
+    }
+  },
+  data() {
+    return {
+      rules: {
+        dirName: [
+          { required: true, message: '请输入文件夹名称', trigger: 'blur' }
+        ],
+        path: [
+          { required: true, message: '请输入文件夹路径', trigger: 'blur' }
+        ],
+        describe: [
+          { required: true, message: '请输入文件夹描述', trigger: 'blur' }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/yunpan/dir/index.vue
@@ -0,0 +1,111 @@
+<template>
+    <div>
+        <el-form :inline="true" :model="queryForm" class="demo-form-inline">
+            <el-form-item label="文件夹名称">
+                <el-input v-model="queryForm.dirName" placeholder="文件夹名称"></el-input>
+            </el-form-item>
+            <el-form-item label="文件夹路径">
+                <el-input v-model="queryForm.path" placeholder="文件夹路径"></el-input>
+            </el-form-item>
+            <el-form-item label="文件夹描述">
+                <el-input v-model="queryForm.describe" placeholder="文件夹描述"></el-input>
+            </el-form-item>
+            <el-form-item label="状态">
+                <el-select v-model="queryForm.status" placeholder="状态">
+                    <el-option label="全部" value=""></el-option>
+                    <el-option label="正常" value="0"></el-option>
+                    <el-option label="禁用" value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="findByCond">查询</el-button>
+                <el-button type="primary" @click="toAdd">新建</el-button>
+            </el-form-item>
+        </el-form>
+
+        <el-table v-loading="isLoading" :data="tableData" highlight-current-row :fit=true style="width: 100%">
+            <el-table-column align="center" prop="id" label="id" width="180" v-if="false"></el-table-column>
+            <el-table-column align="center" prop="dirName" label="文件夹名称" width="180"></el-table-column>
+            <el-table-column align="center" prop="path" label="文件夹路径" width="180"></el-table-column>
+            <el-table-column align="center" prop="describe" label="文件夹描述" width="180"></el-table-column>
+            <el-table-column align="center" label="操作">
+                <template #default="scope">
+                    <el-button size="small" @click="toUpdate(scope.$index, scope.row)">编辑</el-button>
+                    <el-button size="small" @click="toEditDirPermission(scope.$index, scope.row)">权限</el-button>
+                    <el-button size="small" type="danger" @click="toDel(scope.$index, scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+
+
+        <form-dialog title="新建/修改文件夹" ref="dirDialog" :submit-fn="submitFn" :after-submit-fn="findByCond">
+            <template v-slot:body>
+                <dir-form :dirFormData="formData" />
+            </template>
+        </form-dialog>
+
+        <el-dialog title="编辑文件夹" :model-value="dirDirDialogVisible" width="80%" :before-close="handleClose">
+            <dir-permission :id="id" :timeStamp="timeStamp" ref="dirDirRef" />
+
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+import { getCurrentInstance, ref } from "vue";
+import bind from "../../../utils/index.handle";
+
+export default {
+    name: 'index',
+    setup() {
+        const queryForm = ref({
+            name: '',
+            describe: '',
+            status: ''
+        })
+
+        const formData = ref({
+            name: null,
+            describe: null,
+            id: null
+        })
+
+        const { proxy } = getCurrentInstance();
+        const bindHandle = bind({
+            module: 'dir',
+            queryForm: queryForm,
+            formData: formData,
+            dialog: 'dirDialog'
+        }, proxy);
+
+        const dirDirDialogVisible = ref(false)
+        const id = ref(0)
+        const timeStamp = ref(0)
+
+        const toEditDirPermission = (index, row) => {
+            id.value = row.id
+            timeStamp.value = new Date().getTime()
+            dirDirDialogVisible.value = true
+        }
+        const handleClose = () => {
+            dirDirDialogVisible.value = false
+        }
+
+        return {
+            ...bindHandle,
+            queryForm,
+            formData,
+            dirDirDialogVisible,
+            toEditDirPermission,
+            handleClose,
+            id,
+            timeStamp
+        }
+    },
+    computed: {},
+    methods: {}
+}
+</script>
+
+<style scoped>
+</style>
